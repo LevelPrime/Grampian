d7f6d016ab29bb443d6f132dcea0493bddcaeb9c
Roberto Fichera <robyf@tekno-soft.it>
2020-09-09 11:46:05 +0200

    [13.0][website_justsanitise_gtm] Add google tag manager

---
+++ website_justsanitise_gtm/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Website JustSanitise GTM",
+
+    'summary': "Website JustSanitise GTM",
+
+    'description': """Website JustSanitise GTM""",
+
+    "author": "LevelPrime s.r.l.",
+    "category": "web",
+    "website": "https://www.levelprime.com",
+    'version': '0.1',
+    'depends': ['web'],
+
+    'data': [
+              'views/views.xml'
+            ],
+
+    'installable' : True,
+    'auto_install': False,
+    'application' : True,
+}
